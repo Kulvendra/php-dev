6fc0eb8254be0aaf702c0fbb620a94f82563c6c6
kulvendra <kulvendra6749@gmail.com>
2020-10-29 14:11:08 +0530

    Path variable is added

--- private/initialize.php
+++ private/initialize.php
@@ -6,6 +6,11 @@
     define("SHARED_PATH",PRIVATE_PATH.'/shared');
 
 
+    $public_end = strpos($_SERVER['SCRIPT_NAME'],'/public')+7;
+    $doc_root = substr($_SERVER['SCRIOPT_NAME'],0,$public_end);
+    define("WWW_ROOT",$doc_root);
+
+        // sdfsdf
 
     require_once('functions.php');
 ?>
